3a033f7b4908b0bd70a678d37b9295ef19b91a44
CadeMac <cadewilky@gmail.com>
2022-09-11 20:12:28 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 3000);
 
 export default function App() {
+
+
+
+  let calculateTickets = () => {
+    const ticketPrice = 99.99;
+    const [numTickets, setTickets] = UseState(null);
+
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ticket Vault</Text>
+      <TextInput
+        value={numTickets}
+        onChangeText={onChangeNunmber} 
+        keyboardType="numeric"></TextInput>
+      <TouchableOpacity onPress={calculateTickets}>
+        <Text>Find the Cost</Text>
+      </TouchableOpacity>
+      <Text>Total Price: {ticketPrice}</Text>
+
+      <Image source={{ uri: './assets/concert.png' }} style={styles.logo} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +41,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 18
+  },
+  button: {
+    backgroundColor: '#f98b88',
+    justifyContent: 'center'
+
+  }
 });
